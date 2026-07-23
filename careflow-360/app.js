b1db6c7f9052c92c97cd31c9b147e017ec049782
Akhileshal96 <132011661+Akhileshal96@users.noreply.github.com>
2026-07-24 01:03:07 +0530

    Remove misplaced CareFlow files

--- careflow-360/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const dataScript=document.createElement('script');
-dataScript.src='data.js';
-dataScript.onload=()=>{const mainScript=document.createElement('script');mainScript.src='main.js';document.body.appendChild(mainScript);};
-dataScript.onerror=()=>console.error('Unable to load CareFlow data.');
-document.body.appendChild(dataScript);
